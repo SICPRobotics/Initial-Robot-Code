9ad112d3a63e8fe93319e92f39eeb427b6094920
SICP Robotics <SICP Robotics@172.22.11.1>
2016-01-23 13:23:31 -0600

    Motor changes

---
+++ Edward Seley (January 23 2016) 1pm/src/org/usfirst/frc/team5822/robot/Robot.java
@@ -0,0 +1,105 @@
+package org.usfirst.frc.team5822.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends IterativeRobot {
+	RobotDrive myRobotx;
+	RobotDrive myRobotj;
+    JoystickButton motorbutton;
+	//Joystick jstick;
+	Joystick xstick;
+	int autoLoopCounter;
+	boolean button;
+	VictorSP motor;
+	
+	
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    public void robotInit() {
+    	//myRobotx = new RobotDrive(4, 7);
+    	myRobotx = new RobotDrive(0, 1, 2, 3);
+    	xstick = new Joystick(0);
+    	
+    	motorbutton = new JoystickButton(xstick, 1);
+        motor = new VictorSP(4);
+    	
+    	
+    	
+    }
+    
+    /**
+     * This function is run once each time the robot enters autonomous mode
+     */
+    public void autonomousInit() {
+    	autoLoopCounter = 0;
+    }
+
+    /**
+     * This function is called periodically during autonomous
+     */
+    public void autonomousPeriodic() {
+    	if(autoLoopCounter < 100) //Check if we've completed 100 loops (approximately 2 seconds)
+		{
+			myRobotj.drive(-0.5, 0.0); 	// drive forwards half speed
+			autoLoopCounter++;
+			} else {
+			myRobotj.drive(0.0, 0.0); 	// stop robot
+		}
+    }
+    
+    /**
+     * This function is called once each time the robot enters tele-operated mode
+     */
+    public void teleopInit(){
+    	
+    }
+
+    /**
+     * This function is called periodically during operator control
+     */
+    public void teleopPeriodic() {
+        
+    	//motor.set(1);
+    myRobotx.arcadeDrive(xstick);
+    
+    
+    	for (int x = 0; x < 10; x++){
+      
+    		boolean yesorno = motorbutton.get();
+    		if (yesorno == true){
+    		motor.set(1);}
+    
+    if (yesorno == false)
+    motor.set(0);
+    
+    x--;
+    	}
+    
+    	}
+    
+    	
+    	/**;
+     * This function is called periodically during test mode
+     */
+    public void testPeriodic() {
+    	LiveWindow.run();
+    }
+    
+}
